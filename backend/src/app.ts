f45dd96681e28a05449a97553e5c29c9e645f256
Thushan2002 <selvakumarthushanthan4@gmail.com>
2026-09-05 12:17:15 +0530

    env updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
+import cors from 'cors';
 import type { Request, Response } from 'express';
 import express from 'express';
 
+import { env } from './config/env.js';
 import { errorHandler, notFoundHandler } from './middlewares/error.middleware.js';
 import { httpLogger } from './middlewares/http.logger.js';
 import routes from './routes/index.js';
@@ -8,6 +10,7 @@ import routes from './routes/index.js';
 const app = express();
 
 app.disable('x-powered-by');
+app.use(cors({ origin: env.frontendUrl }));
 app.use(express.json());
 app.use(httpLogger);
 
